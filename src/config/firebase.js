b3df1b61635e7e51bea6ddc3b1dc9a3a0fcf1480
diegocardenasg <die.cardenasg@duocuc.cl>
2025-10-28 19:01:58 -0300

    Colecciones de registros y Firebase

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,18 +1,21 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { getAuth } from "firebase/auth";
+
 const firebaseConfig = {
-    apiKey: "AIzaSyC7hNL3As8hgBaBpwTKwsS33GaGsv--zZ8",
-    authDomain: "tiendahuertohogar-72ded.firebaseapp.com",
-    projectId: "tiendahuertohogar-72ded",
-    storageBucket: "tiendahuertohogar-72ded.appspot.com",
-    messagingSenderId: "888478567485",
-    appId: "1:888478567485:web:90abdf6721a7a587ad6e54",
-    measurementId: "G-1R2GTJ3VGR"
-  };
-  
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
+  apiKey: "AIzaSyBQWpFadj7L-U-jF1b1DeEJqX-vDEmyiTA",
+  authDomain: "huertohogar-15d5.firebaseapp.com",
+  projectId: "huertohogar-15d5",
+  storageBucket: "huertohogar-15d5.appspot.com",
+  messagingSenderId: "663380007423",
+  appId: "1:663380007423:web:51638d3581e2453989efca",
+  measurementId: "G-6YRGN9FZLM"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+export const db = getFirestore(app);
+
+export const auth = getAuth(app);
